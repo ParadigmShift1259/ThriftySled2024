52286d7cab29ec9593dca7e50f9335b5afbf58f3
Scott Coursin <scott.coursin@gmail.com>
2025-04-04 17:22:46 -0500

    Fat fingered last commit

--- src/main/include/subsystems/LEDSubsystem.h
+++ src/main/include/subsystems/LEDSubsystem.h
@@ -115,7 +115,7 @@ class LEDSubsystem : public frc2::SubsystemBase
 
     bool m_bAllianceSet = false;
 
-    static constexprble c_defaultSpeed = 0.5;
+    static constexpr double c_defaultSpeed = 0.5;
     static constexpr int c_ledNum = 16;
     static constexpr int c_ledOffset = 8;
 
